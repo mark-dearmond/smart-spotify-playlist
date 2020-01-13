b7750e2e7ee423517c6cafaf433c70299353a66d
Mark DeArmond <bobb.dearmond@gmail.com>
2020-01-13 15:44:51 -0600

    Changed redirect URL

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 
 export const clientId = "ace6423329ad4c7b85e10a16eaf7d198";
-export const redirectUri = "http://localhost:3000/callback";
-// export const redirectUri = "https://mark-dearmond.github.io/smart-spotify-playlist/";
+// export const redirectUri = "http://localhost:3000/callback";
+export const redirectUri = "https://mark-dearmond.github.io/smart-spotify-playlist/";
 export const scopes = [
   "playlist-read-private",
   "playlist-modify-private",
