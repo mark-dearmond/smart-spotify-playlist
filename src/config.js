a4cb2dc7a1b8b51a6ba84c598c9ffd4cdd3248f7
Mark DeArmond <bobb.dearmond@gmail.com>
2020-01-29 10:48:30 -0600

    Removed client id

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 
-export const clientId = "ace6423329ad4c7b85e10a16eaf7d198";
-// export const redirectUri = "http://localhost:3000/callback";
-export const redirectUri = "https://mark-dearmond.github.io/smart-spotify-playlist/";
+export const clientId = "YOUR CLIENT ID";
+export const redirectUri = "http://localhost:3000/callback";
+// export const redirectUri = "https://mark-dearmond.github.io/smart-spotify-playlist/";
 export const scopes = [
   "playlist-read-private",
   "playlist-modify-private",
   "playlist-modify-public",
   "user-top-read"
 ];
-export const baseUrl = "https://api.spotify.com/v1"
+export const baseUrl = "https://api.spotify.com/v1";
