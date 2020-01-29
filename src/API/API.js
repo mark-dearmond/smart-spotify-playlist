e02b3d66c4529a97a87231768153dd1fb76d815a
Mark DeArmond <bobb.dearmond@gmail.com>
2020-01-29 10:48:18 -0600

    refactored to use object and only export API with methods

--- src/API/API.js
+++ src/API/API.js
@@ -1,91 +1,95 @@
-import { baseUrl } from '../config';
+import { baseUrl } from "../config";
 
-export const getUser = async (token) => {
-  const res = await fetch(baseUrl + '/me', {
-    headers: {
-     'Authorization': 'Bearer ' + token
-    }
-  })
-  return await res.json()
-}
+const API = {
+  getUser: async token => {
+    const res = await fetch(baseUrl + "/me", {
+      headers: {
+        Authorization: "Bearer " + token
+      }
+    });
+    return await res.json();
+  },
+  getTopArtists: async token => {
+    const res = await fetch(baseUrl + "/me/top/artists?limit=18", {
+      headers: {
+        Authorization: "Bearer " + token
+      }
+    });
+    return await res.json();
+  },
+  getSearch: async (str, token) => {
+    const query = encodeURIComponent(str);
+    const res = await fetch(
+      baseUrl + "/search?q=" + query + "&type=artist&limit=18",
+      {
+        headers: {
+          Authorization: "Bearer " + token
+        }
+      }
+    );
 
-export const getTopArtists = async (token) => {
-  const res = await fetch(baseUrl + '/me/top/artists?limit=18', {
-    headers: {
-     'Authorization': 'Bearer ' + token
-    }
-  })
-  return await res.json()
-}
+    const results = await res.json();
+    return await results.artists;
+  },
+  getRelatedArtists: async (id, token) => {
+    const res = await fetch(baseUrl + "/artists/" + id + "/related-artists", {
+      headers: {
+        Authorization: "Bearer " + token
+      }
+    });
 
-export const getSearch = async (str, token) => {
-  const query = encodeURIComponent(str);
-  const res = await fetch(baseUrl + '/search?q=' + query + '&type=artist&limit=18', {
-    headers: {
-     'Authorization': 'Bearer ' + token
-    }
-  })
+    const relatedArtists = await res.json();
+    return await relatedArtists.artists;
+  },
+  getTopTracks: async (id, token) => {
+    const res = await fetch(
+      baseUrl + "/artists/" + id + "/top-tracks?country=from_token",
+      {
+        headers: {
+          Authorization: "Bearer " + token
+        }
+      }
+    );
 
-  const results = await res.json();
-  return await results.artists;
-}
+    const topTracks = await res.json();
+    return await topTracks.tracks;
+  },
+  getPlayist: async (id, token) => {
+    const res = await fetch(baseUrl + "/playlists/" + id, {
+      headers: {
+        Authorization: "Bearer " + token
+      }
+    });
 
-export const getRelatedArtists = async (id, token) => {
-  const res = await fetch(baseUrl + '/artists/' + id + '/related-artists', {
-    headers: {
-     'Authorization': 'Bearer ' + token
-    }
-  })
+    const playlist = await res.json();
+    return await playlist;
+  },
 
-  const relatedArtists = await res.json();
-  return await relatedArtists.artists;
-}
+  createPlaylist: async (data, userId, token) => {
+    const res = await fetch(baseUrl + "/users/" + userId + "/playlists", {
+      method: "POST",
+      headers: {
+        Authorization: "Bearer " + token,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    });
+    const playlist = await res.json();
+    return await playlist;
+  },
 
-export const getTopTracks = async (id, token) => {
-  const res = await fetch(baseUrl + '/artists/' + id + '/top-tracks?country=from_token', {
-    headers: {
-     'Authorization': 'Bearer ' + token
-    }
-  })
+  addTracks: async (data, playlistId, token) => {
+    const res = await fetch(baseUrl + "/playlists/" + playlistId + "/tracks", {
+      method: "POST",
+      headers: {
+        Authorization: "Bearer " + token,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    });
+    const playlist = await res.json();
+    return await playlist;
+  }
+};
 
-  const topTracks = await res.json();
-  return await topTracks.tracks;
-}
-
-export const getPlayist = async (id, token) => {
-  const res = await fetch(baseUrl + '/playlists/' + id, {
-    headers: {
-     'Authorization': 'Bearer ' + token
-    }
-  })
-
-  const playlist = await res.json();
-  return await playlist;
-}
-
-export const createPlaylist = async (data, userId, token) => {
-  const res = await fetch(baseUrl + '/users/' + userId + '/playlists', {
-    method: 'POST',
-    headers: {
-     'Authorization': 'Bearer ' + token,
-     'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(data)
-  })
-  const playlist = await res.json();
-  return await playlist;
-}
-
-export const addTracks = async (data, playlistId, token) => {
-  const res = await fetch(baseUrl + '/playlists/' + playlistId + '/tracks', {
-    method: 'POST',
-    headers: {
-     'Authorization': 'Bearer ' + token,
-     'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(data)
-  })
-  const playlist = await res.json();
-  return await playlist;
-}
-
+export default API;
--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from "react";
 import { authEndpoint, clientId, redirectUri, scopes } from "./config";
 import hash from "./Util/hash";
-import { getUser, getTopArtists, getSearch, getRelatedArtists, getTopTracks, createPlaylist, addTracks, getPlayist } from './API/API'
-import logo from './Spotify_Logo.png';
+import API from "./API/API";
+import logo from "./Spotify_Logo.png";
 import "./App.css";
-import Welcome from './Welcome/Welcome';
-import Nav from './Nav/Nav';
-import Artists from './Artists/Artists';
-import Playlist from './Playlist/Playlist'
+import Welcome from "./Welcome/Welcome";
+import Nav from "./Nav/Nav";
+import Artists from "./Artists/Artists";
+import Playlist from "./Playlist/Playlist";
 
-const authUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`;
+const authUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
 
 class App extends Component {
   constructor() {
@@ -25,56 +27,76 @@ class App extends Component {
   async componentDidMount() {
     const token = hash.access_token;
     if (token) {
-      const user = await getUser(token);
-      const artists = await getTopArtists(token);
-      this.setState({token, user, artists});
+      const user = await API.getUser(token);
+      const artists = await API.getTopArtists(token);
+      this.setState({ token, user, artists });
     }
   }
 
-  setArtists = async (query) => {
-    const artists = query && query.length >= 1 ? await getSearch(query, this.state.token) : await getTopArtists(this.state.token)
-    this.setState({artists})
-  }
+  setArtists = async query => {
+    const artists =
+      query && query.length >= 1
+        ? await API.getSearch(query, this.state.token)
+        : await API.getTopArtists(this.state.token);
+    this.setState({ artists });
+  };
 
   buildPlaylist = async (artistId, artistName) => {
     let tracks = [];
-    const selectedArtistTracks = await getTopTracks(artistId, this.state.token);
-    const selectedArtistTrack = await selectedArtistTracks[Math.round(Math.random() * 5)];
-    const relatedArtists = await getRelatedArtists(artistId, this.state.token);
+    const selectedArtistTracks = await API.getTopTracks(
+      artistId,
+      this.state.token
+    );
+    const selectedArtistTrack = await selectedArtistTracks[
+      Math.round(Math.random() * 5)
+    ];
+    const relatedArtists = await API.getRelatedArtists(
+      artistId,
+      this.state.token
+    );
     let relatedArtistsTracks = relatedArtists.map(async artist => {
-      let topTracks = await getTopTracks(artist.id, this.state.token);
+      let topTracks = await API.getTopTracks(artist.id, this.state.token);
       return topTracks[Math.round(Math.random() * 5)];
-    })
+    });
     tracks = await Promise.all(relatedArtistsTracks);
     tracks = await [selectedArtistTrack, ...tracks];
-    const uris = {'uris': tracks.map(track => track.uri)};
-    const createdPlaylist = await createPlaylist({name: artistName + ' Smart Playlist'}, this.state.user.id, this.state.token);
-    await addTracks(uris, createdPlaylist.id, this.state.token);
-    const playlist = await getPlayist(createdPlaylist.id, this.state.token);
-    this.setState({playlist, artists: null});
-  }
+    const uris = { uris: tracks.map(track => track.uri) };
+    const createdPlaylist = await API.createPlaylist(
+      { name: artistName + " Smart Playlist" },
+      this.state.user.id,
+      this.state.token
+    );
+    await API.addTracks(uris, createdPlaylist.id, this.state.token);
+    const playlist = await API.getPlayist(createdPlaylist.id, this.state.token);
+    this.setState({ playlist, artists: null });
+  };
 
   newPlaylist = async () => {
     await this.setArtists();
-    this.setState({playlist: null});
-  }
+    this.setState({ playlist: null });
+  };
 
   render() {
-    const {token, user, artists, playlist} = this.state;
+    const { token, user, artists, playlist } = this.state;
     return (
       <div className="container-fluid">
-        {!token && (
-          <Welcome url={authUrl} logo={logo}></Welcome>
-        )}
+        {!token && <Welcome url={authUrl} logo={logo}></Welcome>}
         {token && (
           <div>
             <Nav user={user}></Nav>
-            {artists && 
-              <Artists artists={artists.items} selectArtist={this.buildPlaylist} onChange={this.setArtists}></Artists>
-            }
-            {playlist &&
-              <Playlist playlist={playlist} newPlaylist={this.newPlaylist}></Playlist>
-            }
+            {artists && (
+              <Artists
+                artists={artists.items}
+                selectArtist={this.buildPlaylist}
+                onChange={this.setArtists}
+              ></Artists>
+            )}
+            {playlist && (
+              <Playlist
+                playlist={playlist}
+                newPlaylist={this.newPlaylist}
+              ></Playlist>
+            )}
           </div>
         )}
       </div>
@@ -83,4 +105,3 @@ class App extends Component {
 }
 
 export default App;
-
